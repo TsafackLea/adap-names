410bef904f126617f5b24e9ea846c67f1284d5c8
TsafackLea <tsafackdominiq@yahoo.com>
2025-11-02 22:02:12 +0100

    Lea

--- src/adap-b02/files2/ObjFile.ts
+++ src/adap-b02/files2/ObjFile.ts
@@ -4,50 +4,60 @@ export class ObjFile implements File {
 
     protected data: Object[] = [];
     protected length: number = 0;
-
+    protected _isOpen: boolean = false;
+  
     public isEmpty(): boolean {
       return this.length == 0;
     }
 
     public isOpen(): boolean {
-      throw new Error("incomplete example code");
+      return this._isOpen;
     }
   
     public isClosed(): boolean {
-        throw new Error("incomplete example code");
+        return !this._isOpen;
     }
   
     public open(): void {
       this.assertIsClosedFile();
-      throw new Error("incomplete example code");
+      this._isOpen = true;
     }
 
     public read(): Object[] {
       this.assertIsOpenFile();
-      throw new Error("incomplete example code");
+      return [...this.data];
     }
 
     public write(data: Object[]): void {
       this.assertIsOpenFile();
-      throw new Error("incomplete example code");
+      if (!Array.isArray(data)) {
+      throw new Error("write(data): Argument sollte ein Array sein.");
+    }
+    this.data.push(...data);
+    this.length = this.data.length;
     }
   
     public close(): void {
       this.assertIsOpenFile();
-      throw new Error("incomplete example code");
+      this._isOpen = false;
     }
 
     public delete(): void {
       this.assertIsClosedFile();
-      throw new Error("incomplete example code");
+      this.data = [];
+      this.length = 0;
     }
 
     protected assertIsOpenFile(): void {
-        throw new Error("incomplete example code");
+       if (!this._isOpen) {
+      throw new Error("Operation nicht erlaubt: Die Datei ist geschlossen.");
+      }
     }
 
     protected assertIsClosedFile(): void {
-        throw new Error("incomplete example code");
+       if (this._isOpen) {
+      throw new Error("Operation nicht erlaubt: Die Datei ist geöffnet.");
+      }
     }
 
 }
